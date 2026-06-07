f2f8c9a67ace7ef5baf9b7fdecf41165a079eb85
Arielxbp <alessandrotang@gmail.com>
2026-06-08 00:37:07 +0200

    wip generalized template for server and db

--- April 2026/2026-04-10-nome-cognome-matricola/3/main.cpp
+++ April 2026/2026-04-10-nome-cognome-matricola/3/main.cpp
@@ -17,10 +17,6 @@ public:
         : pid_(pid), S_(S), P_(P), Q_(Q), A_(A), B_(B), S0_(S0), P0_(P0), Q0_(Q0), bus_(bus), rng_(rng) {}
     
     void initialize(double startTime) override {
-        // Random time + fixed write delay + fixed read delay
-        // Write delay is for the message being sent to the server, which is explicitly modeled.
-        // Read delay is for the implied reply from the server
-        // which is not explicitly modeled but affects the inter-arrival time of customer requests.
         nextTime_ = startTime + rng_.uniform(A_, B_);
     }
 
@@ -29,63 +25,19 @@ public:
     }
 
     void handleEvent(double currentTime) override {
+        // 1-based indexing for s, i, q.  s in [1,S], i in [1,P], q in [1,Q]
+        // 2 * (0..N/2-1) = {2,4,...,N}
+        // 2 * (0..(N+1)/2-1) + 1 = {1,3,...,N}
+        int server = rng_.bernoulli(S0_) ? 2 * rng_.uniformInt(1, S_ / 2) : 2 * rng_.uniformInt(0, (S_ + 1) / 2 - 1) + 1;
+        int item = rng_.bernoulli(P0_) ? 2 * rng_.uniformInt(1, P_ / 2) : 2 * rng_.uniformInt(0, (P_ + 1) / 2 - 1) + 1;
+        int quantity = rng_.bernoulli(Q0_) ? 2 * rng_.uniformInt(1, Q_ / 2) : 2 * rng_.uniformInt(0, (Q_ + 1) / 2 - 1) + 1;
         
-        bool even_if_true;
-
-        // Choose server
-        int current_server_index;
-        rng_.bernoulli(S0_) ? even_if_true = true : even_if_true = false;
-        if (even_if_true) {
-            // Choose only even indexed servers
-            current_server_index = rng_.uniformInt(0, S_ - 1);
-            if (current_server_index % 2 != 0) {
-                current_server_index--; // Adjust to even index
-            }
-        } else {
-            current_server_index = rng_.uniformInt(0, S_ - 1);
-            if (current_server_index % 2 == 0) {
-                current_server_index++; // Adjust to odd index
-            }
-        }
-
-        // Choose item
-        int current_item_index;
-        rng_.bernoulli(P0_) ? even_if_true = true : even_if_true = false;
-        if (even_if_true) {
-            // Choose only even indexed servers
-            current_item_index = rng_.uniformInt(0, P_ - 1);
-            if (current_item_index % 2 != 0) {
-                current_item_index--; // Adjust to even index
-            }
-        } else {
-            current_item_index = rng_.uniformInt(0, P_ - 1);
-            if (current_item_index % 2 == 0) {
-                current_item_index++; // Adjust to odd index
-            }
-        }
-
-        // Choose quantity
-        int current_quantity;
-        rng_.bernoulli(Q0_) ? even_if_true = true : even_if_true = false;
-        if (even_if_true) {
-            // Choose only even indexed servers
-            current_quantity = rng_.uniformInt(1, Q_);
-            if (current_quantity % 2 != 0) {
-                current_quantity--; // Adjust to even index
-            }
-        } else {
-            current_quantity = rng_.uniformInt(1, Q_);
-            if (current_quantity % 2 == 0) {
-                current_quantity++; // Adjust to odd index
-            }
-        }
-
         Message m;
         m.time = currentTime;
         m.sender = pid_;
-        m.receiver = 100 + current_server_index;
-        m.item = current_item_index + 1;
-        m.quantity = current_quantity;
+        m.receiver = 100 + (server - 1); // Server PIDs are [100, 100+S-1]
+        m.item = item;
+        m.quantity = quantity;
         bus_.procToNet(pid_).push(m);
         nextTime_ = currentTime + rng_.uniform(A_, B_);
     }
@@ -103,9 +55,6 @@ public:
         : pid_(pid), database_pid_(database_pid), P_(P), Q_(Q), V_(V), W_(W), bus_(bus), rng_(rng) {}
 
     void initialize(double startTime) override {
-        // Random time + fixed write delay
-        // Write delay is for the message being sent to the server, which is explicitly modeled.
-        // There is no read delay for suppliers since they do not wait for a reply from the DB
         nextTime_ = startTime + rng_.uniform(V_, W_);
     }
     
@@ -133,212 +82,197 @@ private:
 
 class ServerProcess : public DiscreteEventProcess {
 public:
-    ServerProcess(int pid, int C, int database_pid, int P, int Q, double ST0, double ST1, double SEP, double SOP, double QEA, double QOA, double r, std::vector<int> costs, MessageBus &bus, RandomGenerator &rng)
-        : pid_(pid), C_(C), database_pid_(database_pid), P_(P), Q_(Q), ST0_(ST0), ST1_(ST1), SEP_(SEP), SOP_(SOP), QEA_(QEA), QOA_(QOA), r_(r), costs_(costs), bus_(bus), rng_(rng) {
-        cache.assign(P_ + 1, {0, 0}); // Initialize cache with P+1 items (1-based indexing) and (quantity, cost) pairs set to (0,0)
-        sell_buy = 0.0;
+    ServerProcess(int pid, int C, int database_pid, int P, int Q, double ST0, double ST1, double SEP, double SOP, double QEA, double QOA, std::vector<int> costs, MessageBus &bus, RandomGenerator &rng, double serverReadDelay = 0.0, double serverWriteDelay = 0.0)
+        : pid_(pid), C_(C), database_pid_(database_pid), P_(P), Q_(Q), ST0_(ST0), ST1_(ST1), SEP_(SEP), SOP_(SOP), QEA_(QEA), QOA_(QOA), costs_(costs), bus_(bus), rng_(rng), serverReadDelay_(serverReadDelay), serverWriteDelay_(serverWriteDelay), sell_buy_(0.0), y_(0.0) {
+        cache_.assign(P_ + 1, {0, 0}); // Initialize cache with P+1 items (1-based indexing) and (quantity, cost) pairs set to (0,0)
     }
     
     void initialize(double startTime) override {
-        nextTime_ = startTime + r_;
+        nextReadTime_ = startTime + 0.1;
+        inPending_.clear();
 
+        updateCacheTime_ = startTime + (pid_ % 2 == 0 ? ST0_ : ST1_);
+        sell_buy_ = 0.0;
+        y_ = 0.0;
+        // Initialize cost for each item in the cache
         for (int i = 1; i <= P_; ++i) {
-            cache[i].second = costs_[i]; // Initialize cost for each item in the cache
+            cache_[i].second = costs_[i]; 
         }
     }
 
     double nextEventTime() const override {
-        return nextTime_;
+        double t = nextReadTime_;
+        t = std::min(t, updateCacheTime_);
+        for (const auto &m : inPending_) {
+            if (m.deliveryTime < t) {
+                t = m.deliveryTime;
+            }
+        }
+        return t;
     }
     
     void handleEvent(double currentTime) override {
+
+        pendingReady(currentTime);
         
-        // To update cache
-        int item_index;
-        int quantity;
-        if (pid_%2 == 0) {
-            if (currentTime - lastTime_ > ST0_) {
-                bool even_if_true;
-                rng_.bernoulli(SEP_) ? even_if_true = true : even_if_true = false;
-                if (even_if_true) {
-                    // Choose only even indexed items
-                    item_index = rng_.uniformInt(1, P_);
-                    if (item_index % 2 != 0) {
-                        item_index--; // Adjust to even index
-                    }
-                } else {
-                    item_index = rng_.uniformInt(1, P_);
-                    if (item_index % 2 == 0) {
-                        item_index++; // Adjust to odd index
-                    }
-                }
-                rng_.bernoulli(QEA_) ? even_if_true = true : even_if_true = false;
-                if (even_if_true) {
-                    // Choose only even indexed quantities
-                    quantity = rng_.uniformInt(1, Q_);
-                    if (quantity % 2 != 0) {
-                        quantity--; // Adjust to even index
-                    }
-                } else {
-                    quantity = rng_.uniformInt(1, Q_);
-                    if (quantity % 2 == 0) {
-                        quantity++; // Adjust to odd index
-                    }
-                }
-                Message m;
-                m.time = currentTime;
-                m.sender = pid_;
-                m.receiver = database_pid_;
-                m.item = item_index;
-                m.quantity = (double)quantity;
-                bus_.procToNet(pid_).push(m);
+        if (currentTime >= updateCacheTime_) {
 
-                cache[item_index].first += quantity;
-                sell_buy -= quantity * cache[item_index].second;
+            // ...
 
-                nextTime_ = currentTime + r_;
-                lastTime_ = currentTime;
-                return;
-            }
-        } else {
-            if (currentTime - lastTime_ > ST1_) {
-                bool odd_if_true;
-                rng_.bernoulli(SOP_) ? odd_if_true = true : odd_if_true = false;
-                if (odd_if_true) {
-                    // Choose only odd indexed items
-                    item_index = rng_.uniformInt(1, P_);
-                    if (item_index % 2 == 0) {
-                        item_index++; // Adjust to odd index
-                    }
-                } else {
-                    item_index = rng_.uniformInt(1, P_);
-                    if (item_index % 2 == 0) {
-                        item_index++; // Adjust to odd index
-                    }
-                }
-                rng_.bernoulli(QOA_) ? odd_if_true = true : odd_if_true = false;
-                if (odd_if_true) {
-                    // Choose only odd indexed quantities
-                    quantity = rng_.uniformInt(1, Q_);
-                    if (quantity % 2 == 0) {
-                        quantity++; // Adjust to odd index
-                    }
-                } else {
-                    quantity = rng_.uniformInt(1, Q_);
-                    if (quantity % 2 != 0) {
-                        quantity--; // Adjust to even index
-                    }
-                }
-                Message m;
-                m.time = currentTime;
-                m.sender = pid_;
-                m.receiver = database_pid_;
-                m.item = item_index;
-                m.quantity = (double)quantity;
-                bus_.procToNet(pid_).push(m);
-
-                // cache[item_index].first += quantity;
-                // sell_buy -= quantity * cache[item_index].second;
-
-                nextTime_ = currentTime + r_;
-                lastTime_ = currentTime;
-                return;
-            }
+            updateCacheTime_ = currentTime + (pid_ % 2 == 0 ? ST0_ : ST1_);
         }
 
-        // Check the first message in the inbox
-        auto &inbox = bus_.netToProc(pid_);
-
-        if (!inbox.empty()) {
-            Message m = inbox.front(); inbox.pop();
-            
-            if (m.sender == database_pid_) {
-                // This is a reply from the database to a previous request
-                int item_index = m.item;
-                cache[item_index].first += (int)m.quantity;
-                sell_buy -= (int)m.quantity * cache[item_index].second;
-
-                if (nextTime_ < currentTime + 0.1) {
-                    nextTime_ = currentTime + 0.1;
+        if (currentTime >= nextReadTime_) {
+            auto &inbox = bus_.netToProc(pid_);
+            while (!inbox.empty()) {
+                Message m = inbox.front(); inbox.pop();
+                if (m.sender == database_pid_) {
+                    Message reply;
+                    reply.time = currentTime;
+                    reply.sender = pid_;
+                    reply.receiver = m.sender;
+                    reply.item = m.item;
+                    reply.quantity = m.quantity;
+                    if (serverWriteDelay_ <= 0.0) {
+                        bus_.procToNet(reply.receiver).push(reply);
+                    } else {
+                        inPending_.push_back({reply, currentTime + serverWriteDelay_});
+                    }
+                } else {
+                    // Customer message
                 }
-                lastTime_ = currentTime;
+            }
+            /** Alternative to while-loop if there is a server read delay:
+            if (!inbox.empty()) {
+                Message m = inbox.front(); inbox.pop();
+                ...
+                nextReadTime_ = currentTime + serverReadDelay_;
+            } else { 
+                nextReadTime_ = currentTime + 0.1;
+            }
+            */
+
+            // Generalized nextReadTime_ update to handle both cases (with or without server read delay)
+            if (serverReadDelay_ > 0.0) {
+                nextReadTime_ = currentTime + serverReadDelay_;
             } else {
-                // This is a new request from a customer
-                int item_index = m.item;
-
-                int decrement = std::min(cache[item_index].first, (int)m.quantity);
-                
-                cache[item_index].first -= decrement;
-
-                sell_buy += decrement * cache[item_index].second;
-                
-                if (nextTime_ < currentTime + 0.1) {
-                    nextTime_ = currentTime + 0.1;
-                }
-                lastTime_ = currentTime;
+                nextReadTime_ = currentTime + 0.1;
+            }
         }
     }
-}
 
 private:
+
+    struct PendingOutMessage {
+        Message msg;
+        double deliveryTime; // currentTime + server write delay
+    };
+
+    void pendingReady(double currentTime) {
+        auto ip = inPending_.begin();
+        while (ip != inPending_.end()) {
+            if (ip->deliveryTime <= currentTime) {
+                bus_.procToNet(ip->msg.sender).push(ip->msg);
+                ip = inPending_.erase(ip);
+            } else {
+                ++ip;
+            }
+        }
+    }
+
     int pid_, C_, database_pid_, P_, Q_;
+    
     double ST0_, ST1_, SEP_, SOP_, QEA_, QOA_;
-    double r_;
-    double sell_buy;
-    std::vector<std::pair<int, int>> cache; // (quantity, cost)
     std::vector<int> costs_;
+
     MessageBus &bus_;
     RandomGenerator &rng_;
-    double nextTime_;
-    double lastTime_;
+    double nextReadTime_;
+    std::vector<PendingOutMessage> inPending_;
+    double serverReadDelay_, serverWriteDelay_;
+
+    std::vector<std::pair<int, int>> cache_; // (quantity, cost)
+    double sell_buy_;
+    double y_;
+    double updateCacheTime_;
+    
 };
 
 class DatabaseProcess : public DiscreteEventProcess {
 public:
-    DatabaseProcess(int pid, int C,int S, int F, int P, int Q, double r, std::vector<std::pair<int, int>> &inv, MessageBus &bus, RandomGenerator &rng)
-        : pid_(pid), C_(C), S_(S), F_(F), P_(P), Q_(Q), r_(r), inv_(inv), bus_(bus), rng_(rng) {
+    DatabaseProcess(int pid, int C,int S, int F, int P, int Q, double r, std::vector<std::pair<int, int>> &inv, MessageBus &bus, RandomGenerator &rng, double dbReadDelay = 0.0, double dbWriteDelay = 0.0)
+        : pid_(pid), C_(C), S_(S), F_(F), P_(P), Q_(Q), r_(r), inv_(inv), bus_(bus), rng_(rng), dbReadDelay_(dbReadDelay), dbWriteDelay_(dbWriteDelay) {
     }
     void initialize(double startTime) override {
-        nextTime_ = startTime + r_;
+        nextReadTime_ = startTime + 0.1;
+        inPending_.clear();
     }
 
     double nextEventTime() const override {
-        return nextTime_;
+        double t = nextReadTime_;
+        for (const auto &m : inPending_) {
+            if (m.deliveryTime < t) {
+                t = m.deliveryTime;
+            }
+        }
+        return t;
     }
 
     void handleEvent(double currentTime) override {
-        auto &inbox = bus_.netToProc(pid_);
-        if (!inbox.empty()) {
-            Message m = inbox.front(); inbox.pop();
-            bool isSupply = (m.sender >= C_ && m.sender < C_ + F_);
-            if (isSupply) {
-                inv_[m.item].first += (int)m.quantity;
-                nextTime_ = currentTime + r_;
-            } else {
-                // Server message
-                int req = (int)m.quantity;
-                int available = std::min(req, inv_[m.item].first);
-                inv_[m.item].first -= available;
 
-                // Reply to server
-                Message reply;
-                reply.time = currentTime;
-                reply.sender = pid_;
-                reply.receiver = m.sender;
-                reply.item = m.item;
-                reply.quantity = (double)available;
-                bus_.procToNet(pid_).push(reply);
-                nextTime_ = currentTime + r_;
+        pendingReady(currentTime);
+
+        // (If db read delay = true): currentTime + 0.1 - dbReadDelay_
+        // (If db read delay = false): currentTime + 0.1 for periodic polling
+        if (currentTime >= nextReadTime_) {
+            auto &inbox = bus_.netToProc(pid_);
+            // (If db read delay = true): if (!inbox.empty()) {...}
+            // (If db read delay = false): while (!inbox.empty()) {...}
+            while (!inbox.empty()) {
+                Message m = inbox.front(); inbox.pop();
+                bool isSupply = (m.sender >= C_ && m.sender < C_ + F_);
+                if (isSupply) {
+                    inv_[m.item].first += (int)m.quantity;
+                    // (If db read delay = true and has successfully read a message): nextReadTime_ = currentTime
+                    // (If db read delay = false): nextReadTime_ = currentTime + 0.1 for periodic polling
+                    nextReadTime_ = currentTime;
+                    
+                } else {
+                    // Server message
+                }
             }
         }
     }
     
 private:
+
+    struct PendingOutMessage {
+        Message msg;
+        double deliveryTime; // currentTime + db write delay
+    };
+
+    void pendingReady(double currentTime) {
+        auto ip = inPending_.begin();
+        while (ip != inPending_.end()) {
+            if (ip->deliveryTime <= currentTime) {
+                bus_.procToNet(ip->msg.sender).push(ip->msg);
+                ip = inPending_.erase(ip);
+            } else {
+                ++ip;
+            }
+        }
+    }
     int pid_, C_, S_, F_, P_, Q_;
-    double r_, nextTime_;
+
+    double r_;
     std::vector<std::pair<int, int>> &inv_;
+
     MessageBus &bus_;
     RandomGenerator &rng_;
+    double nextReadTime_;
+    std::vector<PendingOutMessage> inPending_;
+    double dbReadDelay_, dbWriteDelay_;
+    
     
 };
 
@@ -409,14 +343,14 @@ int main() {
             system.emplaceProcess<SELib::SupplierProcess>(C+i, database_pid, P, Q, V, W, bus, rng);
         }
         for(int i=0; i<S; ++i) {
-            system.emplaceProcess<SELib::ServerProcess>(100+i, C, database_pid, P, Q, ST0, ST1, SEP, SOP, QEA, QOA, r, costs, bus, rng);
+            system.emplaceProcess<SELib::ServerProcess>(100+i, C, database_pid, P, Q, ST0, ST1, SEP, SOP, QEA, QOA, costs, bus, rng);
         }
         system.emplaceProcess<SELib::DatabaseProcess>(database_pid, C, S, F, P, Q, r, inv, bus, rng);
         // If 0.01 then 200 process -> 200 * 0.01 = 2s of delay just for the router to do one scan of all processes,
         // which is too much. With 0.0001 it's 0.02s which is acceptable for our simulation scale
         // Warn: if set to very low values like 0.00001, the router might cause a bottleneck and
         // significantly increase simulation time due to excessive scanning frequency.
-        system.emplaceProcess<SELib::NetworkRouter>(bus, rng, 0.0001); 
+        system.emplaceProcess<SELib::NetworkRouter>(bus, rng, 0.0001, r); 
 
         // Init DES simulator 
         SELib::DiscreteEventSimulator sim(system);
